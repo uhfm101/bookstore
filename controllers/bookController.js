7516d4e01108924d88cd13923f12ba08cb9a1c4c
uhfm <uhfm@fresnou.org>
2023-02-21 11:35:25 -0800

    book form

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,1 +1,11 @@
-const {Book} = require
+const {Book} = require('../models')
+
+module.exports.viewAll = async function(req, res){
+    const books = await Book.findAll()
+    res.render('book/view_all', {books})
+}
+
+module.exports.viewProfile = async function(req, res){
+    const book = await Book.findByPk(req.params.id)
+    res.render('book/profile', {book})
+}
