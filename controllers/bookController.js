f68ed74a19cc461a9731e736c75ad7c2808b62d4
uhfm <uhfm@fresnou.org>
2023-02-22 11:37:28 -0800

    book functions

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,4 +1,5 @@
 const {Book} = require('../models')
+const genres = ['Novel', 'Fantasy', 'Fiction', 'Adventure Fiction', 'Drama', 'Biography'].sort()
 
 module.exports.viewAll = async function(req, res){
     const books = await Book.findAll()
@@ -8,4 +9,61 @@ module.exports.viewAll = async function(req, res){
 module.exports.viewProfile = async function(req, res){
     const book = await Book.findByPk(req.params.id)
     res.render('book/profile', {book})
+}
+
+module.exports.renderEditForm = async function(req, res){
+    const book = await Book.findByPk(req.params.id)
+        res.render('book/edit', {book, genres})
+}
+
+module.exports.updateBook = async function(req, res){
+    const book = await Book.update({
+        title: req.body.title,
+        author: req.body.author,
+        publisher: req.body.publisher,
+        genre: req.body.genre,
+        pg_number: req.body.pg_number,
+        image: req.body.image,
+        description: req.body.description
+    }, {
+        where: {
+            id: req.params.id
+        }
+    })
+    res.redirect(`/books/profile/${req.params.id}`)
+}
+
+module.exports.renderAddForm = function(req, res){
+    const book = {
+        title: '',
+        genre: genres[0],
+        publisher: '',
+        author: '',
+        pg_number: '',
+        image: '',
+        description: '',
+    }
+    res.render('book/add', {book, genres})
+}
+
+module.exports.addBook = async function(req, res){
+    const book = await Book.create({
+        title: req.body.title,
+        author: req.body.author,
+        publisher: req.body.publisher,
+        genre: req.body.genre,
+        pg_number: req.body.pg_number,
+        image: req.body.image,
+        description: req.body.description
+    })
+    res.redirect(`/books/profile/${book.id}`)
+}
+
+module.exports.deleteBook = async function(req, res){
+    const book = await Book.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    res.redirect('/books')
 }
