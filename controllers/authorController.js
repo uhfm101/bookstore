ea117bd51db71fa9b0655064759781454c3f6736
uhfm <uhfm@fresnou.org>
2023-02-24 11:37:41 -0800

    join table

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -6,11 +6,53 @@ module.exports.viewAll = async function(req, res){
 }
 
 module.exports.viewProfile = async function(req, res){
-    const author = await Author.findByPk(req.params.id)
+    const author = await Author.findByPk(req.params.id, {
+        include: 'books'
+    })
     res.render('author/profile', {author})
 }
 
 module.exports.renderEditForm = async function(req, res){
     const author = await Author.findByPk(req.params.id)
     res.render('author/edit', {author})
+}
+
+module.exports.updateAuthor = async function(req, res){
+    const author = await Author.update({
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        dot: req.body.dot
+    }, {
+        where: {
+            id: req.params.id
+        }
+    })
+    res.redirect(`/authors/profile/${req.params.id}`)
+}
+
+module.exports.renderAddForm = function(req, res){
+    const author = {
+        first_name: '',
+        last_name: '',
+        dot: '',
+    }
+    res.render('author/add', {author})
+}
+
+module.exports.addAuthor = async function(req, res){
+    const author = await Author.create({
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        dot: req.body.dot
+    })
+    res.redirect(`/authors/profile/${author.id}`)
+}
+
+module.exports.deleteAuthor = async function(req, res){
+    const author = await Author.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    res.redirect('/authors')
 }
--- controllers/bookController.js
+++ controllers/bookController.js
@@ -7,7 +7,9 @@ module.exports.viewAll = async function(req, res){
 }
 
 module.exports.viewProfile = async function(req, res){
-    const book = await Book.findByPk(req.params.id)
+    const book = await Book.findByPk(req.params.id, {
+        include: 'authors'
+    })
     res.render('book/profile', {book})
 }
 
